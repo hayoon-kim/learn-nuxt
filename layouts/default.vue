3fdc002aa8460840db2188f23677b36643be6e50
hayoon <dimple9347@gmail.com>
2024-07-21 23:57:45 +0900

    Add Backend Folder from 'https://github.com/joshua1988/learn-nuxt'

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <header>
+      <h1>{{$route.name}}</h1>
+      <NuxtLink to="/">HomePage</NuxtLink>
+      <NuxtLink to="/main">MainPage</NuxtLink>
+      <NuxtLink to="/product">Product Page</NuxtLink>
+    </header>
+    <Nuxt></Nuxt>
+  </div>
+</template>
+
+<script>
+export default {
+    layout: 'error',
+    props: ['error'] // you can set a custom layout for the error page
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
